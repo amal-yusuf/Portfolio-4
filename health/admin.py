62ffc7d7a8660131d97f681b81113cd73f641044
amal-yusuf <amal.yusuf@hotmail.co.uk>
2022-09-06 06:40:11 +0000

    deployment commit

--- health/admin.py
+++ health/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Post, Comment, Category, UpVote, DownVote
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(UpVote)
+class UpVoteAdmin(admin.ModelAdmin):
+    list_display = ('post','user')
+
+
+@admin.register(DownVote)
+class DownVoteAdmin(admin.ModelAdmin):
+    list_display = ('post','user')
--- health/models.py
+++ health/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.urls import reverse 
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300, unique=True)
+   
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    title_tag = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=300, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    featured_image = CloudinaryField('image', default='placeholder')
+    category = models.CharField(max_length=300, default="uncategorised")
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    snippet = models.CharField(max_length=300)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.author)
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+        
+
+class UpVote(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="upvote_user")
+
+
+class DownVote(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="downvote_user")
+
+
